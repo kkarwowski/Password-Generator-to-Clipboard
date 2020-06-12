3dadde23b8f2ca62112b8a3fb92a32e2e9f83059
kkarwowski <crypto_sarge@icloud.com>
2020-06-12 16:31:49 +0100

    added option for complex password generation

--- Password generator.py
+++ Password generator.py
@@ -5,11 +5,16 @@ import os
 import sys
 from random import SystemRandom
 
-menu_def = ['BLANK',['&Generate', 'E&xit']]
+menu_def = ['BLANK',['&Complex','&Standard', 'E&xit']]
 
 
 pass_list = ['Monday','Tuesday','Wednesday','Thursday','Friday','Welcome','Saturday','Sunday']
 
+complex_char = ['$', '&', '*', "(", ')', '@', '!', '%']
+
+def random_numer(a,b):
+    x = SystemRandom().choice(range(a,b))
+    return x
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -23,17 +28,37 @@ def resource_path(relative_path):
 
 
 def generate_rand():
-    srand = SystemRandom()
-    n = len(pass_list)
-    x = srand.choice(range(0,n))
     
-    srand = SystemRandom()
-    num = srand.choice(range(1000, 9999))
+    n = len(pass_list)
+    x = random_numer(0,n)
+    
+    
+    num = random_numer(1000,9999)
 
     rand_word = pass_list[x]+str(num)
     pyperclip.copy(rand_word)
     print(rand_word)
+    
+# Generates complex random password with one of the special characters in random part of the one of the words.
+def generate_rand_complex():
+    n = len(pass_list)
+    x = random_numer(0,n)
 
+    rand_word = pass_list[x]
+    len_of_word = len(rand_word)
+    len_of_word += 1
+
+    y = random_numer(1,len_of_word)
+
+    list_of_char = list(rand_word)
+    random_complex_char = SystemRandom().choice(complex_char)
+    list_of_char.insert(y-1,random_complex_char)
+    new_word = ''.join(list_of_char)
+    num = random_numer(1000,9999)
+    rand_word = new_word+str(num)
+    pyperclip.copy(rand_word)
+
+    print(rand_word)
 
 tray = sg.SystemTray(menu=menu_def, filename=resource_path('Icon.ico'))
 
@@ -41,6 +66,7 @@ while True:  # The event loop
     menu_item = tray.Read()
     if menu_item == 'Exit':
         break
-    elif menu_item == 'Generate':
+    elif menu_item == 'Standard':
         generate_rand()
-        
+    elif menu_item == 'Complex':
+        generate_rand_complex()
